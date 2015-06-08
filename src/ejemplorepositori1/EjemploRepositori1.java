2f930967241dbc10487c8a86638e9601e3599d8f
adiseño.2015 <adiseo.2015@192.168.56.1>
2015-06-08 10:50:44 -0600

    Segundo Hola Mundo en el repositorio para GitHub

--- src/ejemplorepositori1/EjemploRepositori1.java
+++ src/ejemplorepositori1/EjemploRepositori1.java
@@ -18,6 +18,8 @@ public class EjemploRepositori1 {
         // TODO code application logic here
         
         System.out.println("Hola mundo");
+        
+        System.out.println("Hola mundo");
     }
     
 }
