854571f98c8869778c630e9cbd66d78c2738274a
adiseño.2015 <adiseo.2015@192.168.56.1>
2015-06-08 10:48:14 -0600

    Hola mundo en el ejemplo del repositorio en GitHub

--- src/ejemplorepositori1/EjemploRepositori1.java
+++ src/ejemplorepositori1/EjemploRepositori1.java
@@ -16,6 +16,8 @@ public class EjemploRepositori1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hola mundo");
     }
     
 }
